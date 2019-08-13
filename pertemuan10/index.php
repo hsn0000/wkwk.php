087f26cbe5f802dfa3dd24cea6c85f81df977773
MHusin <Muhamad.husin120495@gmail.com>
2019-08-13 13:07:14 +0700

    insert & dellete

---
+++ pertemuan10/index.php
@@ -0,0 +1,60 @@
+<?php
+// koneksi ke database
+require 'functions.php'; 
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Halaman Admin</title>
+
+
+</head>
+<body>
+    
+ <h1>Daftar Mahasiswa</h1>
+
+<a href="tambah.php">Tambah Data Mahasiswa</a>
+<br><br>
+
+<table border="1" cellpadding="10" cellspacing="0">
+  <tr>
+      <th>No.</th>
+      <th>Aksi</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+   </tr>
+
+   <?php $i = 1; ?>
+   <?php foreach( $mahasiswa as $row ) : ?>
+
+<tr>
+    <td><?= $i; ?></td>
+      <td>
+       <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a>
+       <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('apa anda yakin ingin menghapus?'); " >hapus</a>
+     </td>
+    <td class="gambar" ><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
+    <td><?= $row["nrp"]; ?></td>
+    <td><?= $row["nama"]; ?></td>
+    <td><?= $row["email"]; ?></td>
+    <td><?= $row["jurusan"]; ?></td>
+ </tr>
+ <?php $i++;  ?>
+<?php endforeach; ?>
+</table>
+
+</body>
+</html>
